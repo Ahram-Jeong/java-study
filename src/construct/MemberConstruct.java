e6a43c9e85506c6c73433be4edeb6ac0b4ca5a2c
Ahram-Jeong <ahram1215@gmail.com>
2024-11-08 14:36:23 +0900

    20241108 - Constructor : 오버로딩과 this()

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -11,4 +11,20 @@ public class MemberConstruct {
         this.age = age;
         this.grade = grade;
     }
+
+    // 생성자 추가
+    
+    /*
+    MemberConstruct(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.grade = 50;
+    }
+    **/
+
+    MemberConstruct(String name, int age) {
+        // this() : 생성자 내부에서 자신의 생성자 호출 가능
+        // 규칙 : this()는 생성자 코드의 첫 줄에만 작성할 수 있다.
+        this(name, age, 50); // 변경
+    }
 }
