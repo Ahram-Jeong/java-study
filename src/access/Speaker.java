9298eb116641f3b486fba0be99f63fa88234a48d
Ahram-Jeong <ahram1215@gmail.com>
2024-11-13 19:43:08 +0900

    20241113 - 접근 제어자 : 기초 1

---
+++ src/access/Speaker.java
@@ -0,0 +1,27 @@
+package access;
+
+public class Speaker {
+    int volume;
+
+    Speaker(int volume) {
+        this.volume = volume;
+    }
+
+    void volumeUp() {
+        if (volume >= 100) {
+            System.out.println("Max Volume. 음량을 증가할 수 없습니다.");
+        } else {
+            volume += 10;
+            System.out.println("+10 Volume Up");
+        }
+    }
+
+    void volumeDown() {
+        volume -= 10;
+        System.out.println("volumeDown 호출");
+    }
+
+    void showVolume() {
+        System.out.println("Volume : " + volume);
+    }
+}
