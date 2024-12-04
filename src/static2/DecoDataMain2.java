0282f2b10ebdac35b3b4573c4fd77e79a72692ea
Ahram-Jeong <ahram1215@gmail.com>
2024-12-04 21:54:08 +0900

    20241204 - 자바 메모리 구조와 static : static 메소드 3

--- src/static2/DecoDataMain2.java
+++ src/static2/DecoDataMain2.java
@@ -1,26 +1,30 @@
 package static2;
 
-public class DecoDataMain {
+import static static2.DecoData.staticCall;
+
+public class DecoDataMain2 {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall(); // staticValue = 1
+        staticCall();
+        staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출 (1)");
         DecoData data1 = new DecoData();
-        data1.instanceCall(); // instanceValue = 1, staticValue = 2
+        data1.instanceCall();
 
         System.out.println("3. 인스턴스 호출 (2)");
         DecoData data2 = new DecoData();
-        data2.instanceCall(); // instanceValue = 1, staticValue = 3
+        data2.instanceCall();
 
-        DecoData.staticCall(data1); // instanceValue = 2
+        staticCall(data1);
 
         // 추가
         // static 메소드에 인스턴스를 통한 접근 -> 권장 X
         DecoData data3 = new DecoData();
-        data3.staticCall(); // 4
+        data3.staticCall();
 
         // static 메소드에 클래스를 통한 접근 -> 권장 O
-        DecoData.staticCall(); // 5
+        staticCall();
     }
 }
