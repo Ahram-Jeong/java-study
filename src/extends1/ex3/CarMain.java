36c0bbda3c41228cf8e6701b18a302471d917707
Ahram-Jeong <ahram1215@gmail.com>
2024-12-31 22:22:31 +0900

    20241231 - 상속 : 기능 추가와 클래스 확장

---
+++ src/extends1/ex3/CarMain.java
@@ -0,0 +1,21 @@
+package extends1.ex3;
+
+public class CarMain {
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+        electricCar.charge();
+        electricCar.openDoor();
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+        gasCar.fillUp();
+        gasCar.openDoor();
+
+        // 클래스 확장 (수소차 추가)
+        HydrogenCar hydrogenCar = new HydrogenCar();
+        hydrogenCar.move();
+        hydrogenCar.fillHydrogen();
+        hydrogenCar.openDoor();
+    }
+}
