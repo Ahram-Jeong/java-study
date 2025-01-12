44e4772bfa51ffaf39b7c23f6a1529e11a514de4
Ahram-Jeong <ahram1215@gmail.com>
2025-01-12 18:22:20 +0900

    20250112 - 다형성 : 다형성, 메소드 오버라이딩

---
+++ src/poly/overriding/OverridingMain.java
@@ -0,0 +1,23 @@
+package poly.overriding;
+
+public class OverridingMain {
+    public static void main(String[] args) {
+        // 자식 변수가 자식 인스턴스 참조
+        Child child = new Child();
+        System.out.println("Child -> Child");
+        System.out.println("value = " + child.value);
+        child.method(); // Child.method
+
+        // 부모 변수가 부모 인스턴스 참조
+        Parent parent = new Parent();
+        System.out.println("Parent -> Parent");
+        System.out.println("value = " + parent.value);
+        parent.method(); // Parent.method
+
+        // 부모 변수가 자식 인스턴스 참조 (다형적 참조)
+        Parent poly = new Child();
+        System.out.println("Parent -> Child");
+        System.out.println("value = " + poly.value); // value = parent
+        poly.method(); // Child.method -> 오버라이딩을 했기 때문, 오버라이딩 된 메소드는 항상 우선권을 가진다.
+    }
+}
