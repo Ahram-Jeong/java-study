21f07d503609d64dbfff9789df944682f45d4a81
Ahram-Jeong <ahram1215@gmail.com>
2025-01-19 15:39:21 +0900

    20250119 - 다형성 : 인터페이스 2

--- src/poly/ex5/InterfaceAnimal.java
+++ src/poly/ex5/InterfaceAnimal.java
@@ -1,5 +1,9 @@
 package poly.ex5;
 
+/**
+ * 인터페이스는 모든 메소드가 추상 메소드
+ * 인터페이스는 메소드 이름만 있는 설계도이고, 이 설계도가 실제 어떻게 작동하는지는 하위 클래스에서 모두 구현햐애 함 (필수)
+ */
 public interface InterfaceAnimal {
     void sound(); // public abstract 생략된 형태
     void move(); // public abstract 생략된 형태
